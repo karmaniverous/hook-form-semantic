6fe4951634eeb0858f780a9e139e92a47cb4fe8c
Jason Galvin <jasongalvin@gmail.com>
2025-09-25 15:25:24 +0700

    Proper TypeScript declarations for CSS imports

---
+++ playground/src/vite-env.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="vite/client" />
+
+// CSS module declarations
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.less' {
+  const content: string;
+  export default content;
+}
